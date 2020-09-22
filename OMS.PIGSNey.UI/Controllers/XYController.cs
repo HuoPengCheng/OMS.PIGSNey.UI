4f760095f1e72a83f025c439b4918331b29886b7
张鑫鑫 <@>
2020-09-22 10:39:47 +0800

    a

---
+++ OMS.PIGSNey.UI/Controllers/XYController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OMS.PIGSNey.UI.Controllers
+{
+    public class XYController : Controller
+    {
+        public IActionResult MaterialShow()
+        {
+            return View();
+        }
+    }
+}
