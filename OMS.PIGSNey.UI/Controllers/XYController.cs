0716baa73e2e54d0b9b3426bbf9dceb4a7c6f1dc
HPC <HPC@DESKTOP-ST9CDF5>
2020-09-28 16:14:07 +0800

    1

--- OMS.PIGSNey.UI/Controllers/XYController.cs
+++ OMS.PIGSNey.UI/Controllers/XYController.cs
@@ -3,30 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace OMS.PIGSNey.UI.Controllers
 {
     public class XYController : Controller
     {
+        private IConfiguration _configuration;
+
+        public XYController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
         public IActionResult MaterialShow()
         {
+            string url = _configuration["apiUrl"];
+            ViewBag.url = url;
             return View();
         }
 
         public IActionResult ToolShow()
         {
+            string url = _configuration["apiUrl"];
+            ViewBag.url = url;
             return View();
         }
 
        
         public IActionResult MaterialShenHe()
         {
-            
+            string url = _configuration["apiUrl"];
+            ViewBag.url = url;
+
             return View();
         }
 
         public IActionResult ToolShenHe()
         {
+            string url = _configuration["apiUrl"];
+            ViewBag.url = url;
 
             return View();
         }
